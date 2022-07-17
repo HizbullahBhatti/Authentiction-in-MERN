ac69f7bb01d097342b8307947ce2fbd0bc0a3e7a
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2022-07-17 20:01:25 +0500

    JWT joi password complexity and validation and database model

---
+++ server/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+
+    lastName:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    }
+
+});
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id:this._id},process.env.JWTPRIVATEKEY,{expiresIn:"1d"});
+    return token;
+}
+
+const User = mongoose.model('user',userSchema);
+
+const validate = (data)=>{
+    const schema = Joi.object({
+        firstName: Joi.String().required().label("First Name"),
+        lastName: Joi.String().required().label("Last Name"),
+        email: Joi.String().email().required().label("Email"),
+        firstName: passwordComplexity().required().label('Password')
+    });
+    return schema.validate(data);
+}
+
+module.exports = {User,validate}
